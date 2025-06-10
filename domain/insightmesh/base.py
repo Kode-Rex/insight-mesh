800a7bebbc008afaa4057ebe53283c94970319d0
Travis Frisinger <tfrisinger@8thlight.com>
2025-06-10 17:34:04 -0600

    fixed mcp server tests

--- domain/insightmesh/base.py
+++ domain/insightmesh/base.py
@@ -2,7 +2,7 @@
 Base class for InsightMesh domain models.
 """
 
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 # Create base class for InsightMesh domain
 InsightMeshBase = declarative_base() 
--- domain/slack/base.py
+++ domain/slack/base.py
@@ -2,7 +2,7 @@
 Base class for Slack domain models.
 """
 
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 # Create base class for Slack domain
 SlackBase = declarative_base() 
